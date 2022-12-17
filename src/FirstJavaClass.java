8fa6f08ca0c836b4a6ae2ab5306b47b909c5a1f3
zrazzhivin <zrazzhivin@gmail.com>
2022-12-17 13:32:08 +0300

    first homework is done

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,9 +1,8 @@
 public class FirstJavaClass {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
-
 }
